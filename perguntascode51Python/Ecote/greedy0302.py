6e7d326539c7307f2449246b806d1fbcce133f2a
wjunheo <wonjunrs@gmail.com>
2022-11-08 01:51:55 +0900

    08/Nov

--- perguntascode51Python/Ecote/greedy0302.py
+++ perguntascode51Python/Ecote/greedy0302.py
@@ -1,21 +1,60 @@
-n,m,k =map(int,input().split())
-data =list(map(int,input().split()))
+# n,m,k =map(int,input().split())
+# data =list(map(int,input().split()))
+#
+#
+# data.sort()
+# first=data[n-1]
+# second = data[n-2]
+#
+# result =0
+#
+# while True:
+#     for i in range(k):
+#         if m ==0:
+#             break
+#         result += first
+#         m-=1
+#     if m ==0:
+#         break
+#     result += second
+#     m-=1
+# print(result)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n,m,k = map(int,input().split())
+data=list(map(int,input().split()))
+
+answer=0
+count=0
 data.sort()
 first=data[n-1]
-second = data[n-2]
+second=data[n-2]
 
-result =0
 
 while True:
-    for i in range(k):
-        if m ==0:
-            break
-        result += first
-        m-=1
-    if m ==0:
+    if m==0:
         break
-    result += second
+    for i in range(k):
+        answer+=first
+        m-=1
+
+    if m==0:
+        break
+    answer +=second
     m-=1
-print(result)
+print(answer)
